c75cd05da4dd7e43f39403d8e0bc036633f199ef
Gauna Esteban <userprogrammer2023@gmail.com>
2024-02-14 02:35:28 -0300

    PreEntrega1 Gauna

---
+++ ecommerce/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import Carrito from './assets/Carrito.png'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={Carrito} alt="Carrito-wdiget"/>
+            0
+        </div>
+    )
+}
+
+export default CartWidget
